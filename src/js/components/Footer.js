d42d6828868c87b7a7570eb672807ea4c8af0ff0
Phil George <23001002+pg328@users.noreply.github.com>
2024-05-20 17:46:09 +0100

    2024 Update Conversion to Astro

--- src/js/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = (props) => {
-    return <div className={"footer"}>
-        {props.content ? props.content : "Footer Content"}
-    </div>
-};
-
-export default Footer;
